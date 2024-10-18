e6cbefd4666135a45b73cfc555fef7ed48b77c50
gangumi <115536473+gangumi@users.noreply.github.com>
2024-10-18 08:51:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,34 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+let squadre = {
+    'Squadra A': 0,
+    'Squadra B': 0
+};
+
+app.post('/addResult', (req, res) => {
+    const { teamA, teamB, scoreA, scoreB } = req.body;
+
+    if (squadre[teamA] !== undefined && squadre[teamB] !== undefined) {
+        squadre[teamA] += scoreA;
+        squadre[teamB] += scoreB;
+        res.sendStatus(200);
+    } else {
+        res.status(400).send('Una delle squadre non esiste');
+    }
+});
+
+app.get('/getClassifica', (req, res) => {
+    const classifica = Object.keys(squadre).map(team => ({
+        name: team,
+        points: squadre[team]
+    }));
+    res.json(classifica);
+});
+
+app.listen(3000, () => {
+    console.log('Server avviato sulla porta 3000');
+});
 
